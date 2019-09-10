9552b404c35a4b1abf6ec25ffaa35cfe49e2734b
Erika Miguel-Yeo <erika@bocoup.com>
2019-09-10 11:20:19 -0400

    scripts location

--- scripts/start-deployment.sh
+++ scripts/start-deployment.sh
@@ -4,7 +4,7 @@ if [ -z "${DATABASE_URL}" ]; then
     export $(cat config/dev)
 else
     echo "Exporting PG variables..."
-    . ./parse-and-export-pgvars.sh
+    . scripts/parse-and-export-pgvars.sh
 fi
 
 yarn workspace server run start
