197f0f61df8e41193e80145cb96ce45c74a12635
Erika Miguel-Yeo <erika@bocoup.com>
2019-09-10 13:26:48 -0400

    returning heroku db config

--- src/server/util/dbConfig.js
+++ src/server/util/dbConfig.js
@@ -3,10 +3,19 @@ const devConfig = require('../../../config/devDbConfig.json')
 require('dotenv').config();
 
 exports.getDbConfig = function() {
+    // This is the environment variable Heroku exposes
     if (process.env.DATABASE_URL) {
-        const url = parse(process.env.DATABASE_URL, true);
-        console.log(url)
-        return
+        const { username: user, password, hostname: host, port, pathname} = parse(process.env.DATABASE_URL, true);
+        return {
+            user,
+            password,
+            host,
+            port,
+            // the database name is included in the pathname
+            //  the pathname string starts with a  '/', so to
+            //  get the database name, take the subtring
+            database: pathname.substring(1)
+        }
     }
     
     return devConfig;
